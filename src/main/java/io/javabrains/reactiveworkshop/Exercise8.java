85e788e2daab75d10c57eb2d5f25e66b1d3ced4d
Lester Deeley <lester.deeley@yahoo.com>
2023-01-20 13:58:58 +0000

    Initial+ Operators + FlatMap + final

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,19 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFluxWithException()
+//                .subscribe(System.out::println, (e) -> System.out.println(e.getMessage()));
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFluxWithException()
+//                .onErrorContinue((err, item) -> System.out.println(err.getMessage()))
+//                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume((err -> Flux.just(-1, -2)))
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
